69374184f0387af73515b944b8dc317ff30a42fc
Giancarlo Gamarra <youthlinksdev@sos-kd.org>
2018-11-08 11:35:45 -0500

    new commit

---
+++ AccesoDatos/PacientesDAL.cs
@@ -0,0 +1,14 @@
+﻿using EntidadesNegocio;
+using System.Reflection;
+
+namespace AccesoDatos
+{
+    public class PacientesDAL
+    {
+        public static string ConexionPacientes() {
+            string pathBDPacientes= System.IO.Path.GetDirectoryName(
+                Assembly.GetEntryAssembly().Location);
+            return pathBDPacientes;
+        }
+    }
+}
---
+++ SGH/Program.cs
@@ -0,0 +1,46 @@
+﻿using EntidadesNegocio;
+using LogicaNegocios;
+using System;
+
+namespace SGH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PacienteBL pacienteBL = new PacienteBL();
+            var Pacientes= pacienteBL.GetPacientes();
+            foreach (var item in Pacientes)
+            {
+                var i = "";
+                Console.WriteLine($"{item.Dni},{item.Nombre},{item.Apellido}");
+            }
+
+
+            //ACTUALIZAR PACIENTE
+            Paciente updatedPaciente = new Paciente("44216617","Jose", "Herrera");
+            pacienteBL.UpdatePaciente("44216617", updatedPaciente);
+            Console.WriteLine("Actualizacion de Paciente...");
+            foreach (var item in PacienteBL.Listpacientes)
+            {
+                Console.WriteLine($"{item.Dni},{item.Nombre},{item.Apellido}");
+            }
+            //AGREGAR PACIENTE
+            Paciente newPaciente = new Paciente("44216619", "Martin", "Centeno");
+            pacienteBL.InsertPaciente(newPaciente);
+            Console.WriteLine("Insertar Paciente...");
+            foreach (var item in PacienteBL.Listpacientes)
+            {
+                Console.WriteLine($"{item.Dni},{item.Nombre},{item.Apellido}");
+            }
+            //ELIMINAR PACIENTE
+            pacienteBL.DeletePaciente("44216619");
+            Console.WriteLine("Eliminar Paciente...");
+            foreach (var item in PacienteBL.Listpacientes)
+            {
+                Console.WriteLine($"{item.Dni},{item.Nombre},{item.Apellido}");
+            }
+            Console.WriteLine("new change");
+        }
+    }
+}
