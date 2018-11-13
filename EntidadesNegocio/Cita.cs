a710ccd6ebbd3a3fc2830e249b4082ba5b4368b4
JOSECHURATA <JOSEMIGUEL0294@GMAIL.COM>
2018-11-13 17:35:24 -0500

    actualizando clase (cita)

--- EntidadesNegocio/Cita.cs
+++ EntidadesNegocio/Cita.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace EntidadesNegocio
 {
-    class CIta
+    class Cita
     {
-        public int MyProperty { get; set; }
+        public int Idcita { get; set; }
+        public string tipocita { get; set; }
+        public string estadocita { get; set; }
+
+        public Cita( int Idcita,string tipocita, string estadocita)
+        {
+            this.Idcita = Idcita;
+            this.tipocita = tipocita;
+            this.estadocita = estadocita;
+
+        }
     }
 }
